ee7183cf6ac1b219adcec26f78746fdb3220b0e5
Sergio Conti <seracon@gmail.com>
2023-01-22 11:07:31 -0300

    Comienzo de la 2da Parte
    Comienzo de la Segunda parte del Taller de JS

---
+++ 7 - Windows/codigo.js
@@ -0,0 +1,11 @@
+//Con esto puedo hace que me diga lo que me desplace en X
+const scrollX = window.scrollX;
+
+//Con esto puedo hace que me diga lo que me desplace en Y
+const scrollY = window.scrollY;
+
+//Con este comando puedo hacer que me muestre la direccion en
+//donde esta alojado el archivo
+const href = window.location.href;
+
+document.write(href);

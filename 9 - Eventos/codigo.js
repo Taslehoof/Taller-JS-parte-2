48052decdf2edad5eafd1a19df423d03877ed916
Sergio Conti <seracon@gmail.com>
2023-01-29 23:51:52 -0300

    Contenido nuevo
    Se agrego nuevos eventos y nuevas funcionalidades

--- 9 - Eventos/codigo.js
+++ 9 - Eventos/codigo.js
@@ -1,7 +1,11 @@
 const button = document.querySelector(".button");
+const contenedor = document.querySelector(".seleccionado");
+const input = document.querySelector(".input-prueba");
 
 //Esta opcion es la que se esta usando para hacer eventos
-button.addEventListener("click", saludar);
+//button.addEventListener("click", saludar);
+
+//contenedor.addEventListener("click",saludar2);
 
 //Segunda manera en la que puedo manejar los eventos.(DESUSO)
 /*button.onclick = ()=>{
@@ -9,7 +13,37 @@ button.addEventListener("click", saludar);
 }*/
 
 //Esta es la manera correcta de de manejar los eventos
-function saludar(){
-    alert("saludar desde los eventos");
+/*function saludar(){
+    alert("di click en un boton");
     button.removeEventListener("click", saludar);
-}
+}*/
+
+/* 
+*   EVENTOS DE TECLADO
+*/
+
+/*
+input.addEventListener("keydown",(e)=>{
+    console.log("Una tecla fue presionada");
+});
+
+input.addEventListener("keypress",(e)=>{
+    console.log("Un usuario presiono una tecla");
+});
+
+input.addEventListener("keyup",(e)=>{
+    console.log("Una tecla fue soltada");
+});*/
+
+/* 
+*  INTERFAZ DE EVENTOS 
+*/
+
+//Con estas test lineas de codigo puedo capturar y mostrar en pantalla
+//lo que selecciono el usuario
+input.addEventListener("select",(e)=>{
+    let start = e.target.selectionStart;
+    let end = e.target.selectionEnd;
+    let textoCompleto = input.value;
+    contenedor.innerHTML = textoCompleto.substring(start,end);
+})
